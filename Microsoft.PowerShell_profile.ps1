e38ccdc72c697da480715e5d3d8cd5eb062edc9b
raghav18gupta <18raghavgupta@gmail.com>
2021-07-10 21:21:29 +0530

    Initial commit

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,11 +4,12 @@
 ##### Other Settings #####
 Get-PSReadlineKeyHandler -Bound | ? Function -eq ClearScreen\
 Import-Module PSReadLine
+Set-Location ~\Projects
 
 ##### Oh My Posh! #####
 Import-Module oh-my-posh
 Import-Module posh-git
-Set-PoshPrompt wopian
+Set-PoshPrompt powerlevel10k_rainbow
 
 ##### Alias #####
 Set-Alias -Name code -Value code-insiders
